d35bd6f92bc4bd3b41c6a81474fb491fc35c7bd0
mtj0928 <mtj0928@users.noreply.github.com>
2024-05-14 10:03:57 +0900

    Add Script

---
+++ Scripts/release-artifactbundle.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+VERSION_STRING="$1"
+
+mkdir -p nest.artifactbundle/nest-$VERSION_STRING-macos/bin
+
+sed 's/__VERSION__/$VERSION_STRING/g' ./Scripts/info.json > "nest.artifactbundle/info.json"
+
+cp -f ".build/release/nest" "nest.artifactbundle/nest-$VERSION_STRING-macos/bin"
+
+zip -yr - "nest.artifactbundle" > "./nest-macos.artifactbundle.zip"
