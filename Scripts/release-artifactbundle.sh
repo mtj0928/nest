9f688bcaa4b6b9609b7fb56db72545e0a05f7af4
mtj0928 <mtj0928@users.noreply.github.com>
2024-05-14 10:16:55 +0900

    Fix script

--- Scripts/release-artifactbundle.sh
+++ Scripts/release-artifactbundle.sh
@@ -4,7 +4,7 @@ VERSION_STRING="$1"
 
 mkdir -p nest.artifactbundle/nest-$VERSION_STRING-macos/bin
 
-sed 's/__VERSION__/$VERSION_STRING/g' ./Scripts/info.json > "nest.artifactbundle/info.json"
+sed "s/__VERSION__/$VERSION_STRING/g" ./Scripts/info.json > "nest.artifactbundle/info.json"
 
 cp -f ".build/release/nest" "nest.artifactbundle/nest-$VERSION_STRING-macos/bin"
 
