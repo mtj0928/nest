f14a213707dd38d93760e76c3d2e706778192898
mtj0928 <mtj0928@users.noreply.github.com>
2024-05-15 22:48:09 +0900

    Fix test

--- Tests/NestKitTests/NestDirectoryTests.swift
+++ Tests/NestKitTests/NestDirectoryTests.swift
@@ -11,6 +11,36 @@ struct NestDirectoryTests {
         #expect(nestDirectory.bin.relativeString == ".nest/bin")
         #expect(nestDirectory.artifacts.relativeString == ".nest/artifacts")
 
-        #expect(nestDirectory.repository(gitURL: .url(URL(string: "https://github.com/xxx/yyy")!)).relativePath == ".nest/artifacts/xxx_yyy_github.com_https")
+        #expect(
+            nestDirectory.source(
+                .artifactBundle(
+                    sourceInfo: ArtifactBundleSourceInfo(
+                        zipURL: URL(string: "https://example.com/xxx/yyy.zip")!,
+                        repository: Repository(
+                            reference: .url(URL(string: "https://github.com/xxx/yyy")!),
+                            version: "0"
+                        )
+                    )
+                )
+            ).relativePath == ".nest/artifacts/xxx_yyy_github.com_https"
+        )
+        #expect(
+            nestDirectory.source(
+                .artifactBundle(
+                    sourceInfo: ArtifactBundleSourceInfo(
+                        zipURL: URL(string: "https://example.com/xxx/yyy.zip")!,
+                        repository: nil
+                    )
+                )
+            ).relativePath == ".nest/artifacts/xxx_yyy.zip_example.com_https"
+        )
+        #expect(
+            nestDirectory.source(
+                .localBuild(repository: Repository(
+                    reference: .url(URL(string: "https://github.com/xxx/yyy")!),
+                    version: "0"
+                ))
+            ).relativePath == ".nest/artifacts/xxx_yyy_github.com_https"
+        )
     }
 }
