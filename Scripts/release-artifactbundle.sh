3aa575a96e74e4b4d188b20be218302601c24713
mtj0928 <mtj0928@users.noreply.github.com>
2024-06-07 08:52:16 +0900

    Fix script

--- Scripts/release-artifactbundle.sh
+++ Scripts/release-artifactbundle.sh
@@ -6,6 +6,6 @@ mkdir -p nest.artifactbundle/nest-$VERSION_STRING-macos/bin
 
 sed "s/__VERSION__/$VERSION_STRING/g" ./Scripts/info.json > "nest.artifactbundle/info.json"
 
-cp -f ".build/release/nest" "nest.artifactbundle/nest-$VERSION_STRING-macos/bin"
+cp -f "./.build/apple/Products/Release/nest" "nest.artifactbundle/nest-$VERSION_STRING-macos/bin"
 
 zip -yr - "nest.artifactbundle" > "./nest-macos.artifactbundle.zip"
