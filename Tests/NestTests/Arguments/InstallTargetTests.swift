3152290ec17dbdea0ec118a1263a99a92ff7cff5
noppe <noppelabs@gmail.com>
2024-06-15 13:42:38 +0900

    Update Tests/NestTests/Arguments/InstallTargetTests.swift
    Co-authored-by: matsuji <mtj0928@users.noreply.github.com>

--- Tests/NestTests/Arguments/InstallTargetTests.swift
+++ Tests/NestTests/Arguments/InstallTargetTests.swift
@@ -7,13 +7,10 @@ struct InstallTargetTests {
     @Test
     func testInstallTarget() throws {
         let installTarget = InstallTarget(argument: "artifactBundle.zip")
-        switch installTarget {
-        case .git(let gitURL):
-            fatalError()
-        case .artifactBundle(let url):
-            #expect(url == URL(string: "artifactBundle.zip"))
-        case nil:
-            fatalError()
+        guard case .artifactBundle(let url) = installTarget else {
+            Issue.record("installTarget needs to be artifactBundle.")
+            return
         }
+        #expect(url == URL(string: "artifactBundle.zip"))
     }
 }
