b1f3e06b0085b60c6d7a8d516ad26d79694f30dd
mtj0928 <mtj0928@users.noreply.github.com>
2024-10-17 08:35:27 +0900

    Add documents to ArtifactBundleInfo

--- Sources/NestKit/ArtifactBundle/ArtifactBundleInfo.swift
+++ Sources/NestKit/ArtifactBundle/ArtifactBundleInfo.swift
@@ -1,8 +1,12 @@
 import Foundation
 
 /// A data structure corresponding to info.json in artifact bundle.
+///
+///[Definition in Swift evolution]( https://github.com/swiftlang/swift-evolution/blob/main/proposals/0305-swiftpm-binary-target-improvements.md#artifact-bundle-manifest)
 public struct ArtifactBundleInfo: Codable, Hashable, Sendable {
     public var schemaVersion: String
+    /// A map of artifacts. The key is an identifier of an artifact.
+    /// In most cases, the identifier is the same command name.
     public var artifacts: [String: Artifact]
 
     public init(schemaVersion: String, artifacts: [String : Artifact]) {
@@ -12,8 +16,14 @@ public struct ArtifactBundleInfo: Codable, Hashable, Sendable {
 }
 
 public struct Artifact: Codable, Hashable, Sendable {
+    /// A version of the asrtifact
     public var version: String
+
+    /// A type of the artifact.
+    /// Current only "executable" is passed.
     public var type: String
+
+    // Variants of the artifact.
     public var variants: Set<ArtifactVariant>
 
     public init(version: String, type: String, variants: Set<ArtifactVariant>) {
@@ -23,8 +33,12 @@ public struct Artifact: Codable, Hashable, Sendable {
     }
 }
 
+/// A data structure representing an executable in an artifact bundle.
 public struct ArtifactVariant: Codable, Hashable, Sendable {
+    /// A path to the executable file
     public var path: String
+
+    /// A tripes which the executable supports
     public var supportedTriples: [String]
 
     public init(path: String, supportedTriples: [String]) {
