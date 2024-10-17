0fe300d9c37b99659f1ef3bbd2ec0d1e6f3818d9
mtj0928 <mtj0928@users.noreply.github.com>
2024-10-17 09:27:52 +0900

    Improve tests for NestDirectory

--- Tests/NestKitTests/NestDirectoryTests.swift
+++ Tests/NestKitTests/NestDirectoryTests.swift
@@ -3,44 +3,73 @@ import Testing
 @testable import NestKit
 
 struct NestDirectoryTests {
-    @Test
-    func testNestDirectory() throws {
-        let rootDirectory = URL(fileURLWithPath: ".nest")
-        let nestDirectory = NestDirectory(rootDirectory: rootDirectory)
+    let nestDirectory = NestDirectory(rootDirectory: URL(fileURLWithPath: ".nest"))
 
+    @Test
+    func nestDirectory() async throws {
+        #expect(nestDirectory.infoJSON.relativeString == ".nest/info.json")
         #expect(nestDirectory.bin.relativeString == ".nest/bin")
         #expect(nestDirectory.artifacts.relativeString == ".nest/artifacts")
+    }
 
-        #expect(
-            nestDirectory.source(
-                .artifactBundle(
-                    sourceInfo: ArtifactBundleSourceInfo(
-                        zipURL: URL(string: "https://example.com/xxx/yyy.zip")!,
-                        repository: Repository(
-                            reference: .url(URL(string: "https://github.com/xxx/yyy")!),
-                            version: "0"
-                        )
-                    )
-                )
-            ).relativePath == ".nest/artifacts/xxx_yyy_github.com_https"
-        )
-        #expect(
-            nestDirectory.source(
-                .artifactBundle(
-                    sourceInfo: ArtifactBundleSourceInfo(
-                        zipURL: URL(string: "https://example.com/xxx/yyy.zip")!,
-                        repository: nil
-                    )
-                )
-            ).relativePath == ".nest/artifacts/xxx_yyy.zip_example.com_https"
-        )
-        #expect(
-            nestDirectory.source(
-                .localBuild(repository: Repository(
-                    reference: .url(URL(string: "https://github.com/xxx/yyy")!),
+    @Test
+    func artifactsURLForArtifactBundleInRepository() throws {
+        let artifactBundle = try ExecutableManufacturer.artifactBundle(
+            sourceInfo: ArtifactBundleSourceInfo(
+                zipURL: #require(URL(string: "https://example.com/foo/bar.zip")),
+                repository: Repository(
+                    reference: .url(#require(URL(string: "https://github.com/owner/name"))),
                     version: "0"
-                ))
-            ).relativePath == ".nest/artifacts/xxx_yyy_github.com_https"
+                )
+            )
         )
+        #expect(nestDirectory.source(artifactBundle).relativePath == ".nest/artifacts/owner_name_github.com_https")
+
+        let binaryDirectory = nestDirectory.binaryDirectory(manufacturer: artifactBundle, version: "0")
+        #expect(binaryDirectory.relativePath == ".nest/artifacts/owner_name_github.com_https/0/bar")
+    }
+
+    @Test
+    func artifactsURLForZIPURL() throws {
+        let artifactBundle = try ExecutableManufacturer.artifactBundle(
+            sourceInfo: ArtifactBundleSourceInfo(
+                zipURL: #require(URL(string: "https://example.com/foo/bar.zip")),
+                repository: nil
+            )
+        )
+        #expect(nestDirectory.source(artifactBundle).relativePath == ".nest/artifacts/foo_bar.zip_example.com_https")
+        let binaryDirectory = nestDirectory.binaryDirectory(manufacturer: artifactBundle, version: "0")
+        #expect(binaryDirectory.relativePath == ".nest/artifacts/foo_bar.zip_example.com_https/0/bar")
+    }
+
+    @Test
+    func artifactsURLForLocalBuild() throws {
+        let artifactBundle = try ExecutableManufacturer.localBuild(repository: Repository(
+            reference:  .url(#require(URL(string: "https://github.com/xxx/yyy"))),
+            version: "0"
+        ))
+        #expect(nestDirectory.source(artifactBundle).relativePath == ".nest/artifacts/xxx_yyy_github.com_https")
+        let binaryDirectory = nestDirectory.binaryDirectory(manufacturer: artifactBundle, version: "0")
+        #expect(binaryDirectory.relativePath == ".nest/artifacts/xxx_yyy_github.com_https/0/local_build")
+    }
+
+    @Test
+    func version() throws {
+        let artifactBundle = try ExecutableManufacturer.localBuild(repository: Repository(
+            reference:  .url(#require(URL(string: "https://github.com/xxx/yyy"))),
+            version: "0"
+        ))
+        let result = nestDirectory.version(manufacturer: artifactBundle, version: "0.0.1")
+        #expect(result.relativePath == ".nest/artifacts/xxx_yyy_github.com_https/0.0.1")
+    }
+
+    @Test
+    func url() {
+        #expect(nestDirectory.url("foo/bar").relativePath == ".nest/foo/bar")
+    }
+
+    @Test
+    func symbolicPath() {
+        #expect(nestDirectory.symbolicPath(name: "foo").relativePath == ".nest/bin/foo")
     }
 }
