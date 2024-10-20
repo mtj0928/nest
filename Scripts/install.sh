b440175387a44aa2d0fc564e6f97cc37449e440f
mtj0928 <mtj0928@users.noreply.github.com>
2024-10-20 13:58:10 +0900

    Update script

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -2,24 +2,20 @@
 
 REPO="mtj0928/nest"
 ASSET_NAME="nest-macos.artifactbundle.zip"
-API_URL="https://api.github.com/repos/$REPO/releases/latest"
-
-# Fetch release info
-RELEASE_INFO=$(curl -s $API_URL)
-ASSET_URL=$(echo "$RELEASE_INFO" | grep "browser_download_url.*$ASSET_NAME" | head -n 1 | sed -E 's/.*"([^"]+)".*/\1/')
-VERSION=$(echo "$RELEASE_INFO" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+ASSET_URL="https://github.com/$REPO/releases/latest/download/$ASSET_NAME"
 
 # Download zip file
-if [ -n "$ASSET_URL" ]; then
-    curl -sL -o $ASSET_NAME $ASSET_URL
-    unzip -qo $ASSET_NAME -d extracted_files
-    rm $ASSET_NAME
-    ./extracted_files/nest.artifactbundle/nest-$VERSION-macos/bin/nest install mtj0928/nest > /dev/null
-    rm -rf extracted_files
-    echo "🪺 nest was installed at ~/.nest/bin"
-    echo "🪺 Please add it to \$PATH"
-else
-    echo "Failed to install"
-    exit 1
+curl -sL -o $ASSET_NAME $ASSET_URL
+unzip -qo $ASSET_NAME -d extracted_files
+rm $ASSET_NAME
+
+VERSION=$(ls ./extracted_files/nest.artifactbundle | sed -n 's/^nest-\([^-]*\)-macos$/\1/p' | head -n 1)
+if [ -z "$VERSION" ]; then
+  echo "Version not found in the directory."
+  exit 1
 fi
 
+./extracted_files/nest.artifactbundle/nest-$VERSION-macos/bin/nest install mtj0928/nest > /dev/null
+rm -rf extracted_files
+echo "🪺 nest was installed at ~/.nest/bin"
+echo "🪺 Please add it to \$PATH"
