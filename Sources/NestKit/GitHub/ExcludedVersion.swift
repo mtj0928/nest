7fdac4082f2b8cd78e6a48d30ba836761e25b250
coffmark <come1234560@gmail.com>
2025-04-07 07:27:52 +0900

    Move `init?(argument: String)` to the `nest` module
    ref. https://github.com/mtj0928/nest/pull/33#discussion_r2030014081

--- Sources/NestKit/GitHub/ExcludedVersion.swift
+++ Sources/NestKit/GitHub/ExcludedVersion.swift
@@ -5,13 +5,7 @@ public struct ExcludedVersion: Equatable, Sendable {
     public let reference: String
     public let target: String?
 
-    public init?(argument: String) {
-        let split = argument.split(separator: "@")
-        guard split.count == 1 || split.count == 2 else { return nil }
-        self = .init(reference: String(split[0]), target: split.count == 2 ? String(split[1]) : nil)
-    }
-
-    init(reference: String, target: String?) {
+    public init(reference: String, target: String?) {
         self.reference = reference
         self.target = target
     }
--- Sources/nest/Arguments/ExcludedVersion+Arguments.swift
+++ Sources/nest/Arguments/ExcludedVersion+Arguments.swift
@@ -1,4 +1,10 @@
 import ArgumentParser
 import NestKit
 
-extension ExcludedVersion: ExpressibleByArgument { }
+extension ExcludedVersion: ExpressibleByArgument {
+    public init?(argument: String) {
+        let split = argument.split(separator: "@")
+        guard split.count == 1 || split.count == 2 else { return nil }
+        self = .init(reference: String(split[0]), target: split.count == 2 ? String(split[1]) : nil)
+    }
+}
